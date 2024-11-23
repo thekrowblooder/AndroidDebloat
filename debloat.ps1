be7719dd911a9295a81c0f4e5f5ed75d016761f9
Óscar Yyeyyoe <85049592+thekrowblooder@users.noreply.github.com>
2024-11-22 21:26:56 -0600

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,16 +1,13 @@
-$zipPath = "C:\Users\TheKrowBlooder\Desktop\AndroidDebloat.zip"
+$zipUrl = "https://github.com/thekrowblooder/AndroidDebloat/raw/refs/heads/main/AndroidDebloat.zip"
+$zipPath = [System.IO.Path]::Combine($env:TEMP, "AndroidDebloat.zip")
 $tempFolder = [System.IO.Path]::Combine($env:TEMP, "thekrowblooder")
-
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 if (Test-Path $tempFolder) {
     Remove-Item -Path $tempFolder -Recurse -Force
 }
-
 New-Item -Path $tempFolder -ItemType Directory
-
 Expand-Archive -Path $zipPath -DestinationPath $tempFolder -Force
-
 $unistallerScript = Get-ChildItem -Path $tempFolder -Recurse -Filter "unistallerbeta.ps1" | Select-Object -First 1
-
 if ($unistallerScript) {
     & $unistallerScript.FullName
 } else {
